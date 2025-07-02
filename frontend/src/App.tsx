84893935fb0dcfad0a4ec7c6fcf9dd881b1f9d12
igormooura <igor.oliveira.moura@gmail.com>
2025-07-02 16:25:56 -0300

    feat: adicionei a página de admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import { Userpage } from "./pages/UserPage"
+import { AdminPage } from "./pages/AdminPage"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/user' element={<Userpage/>}/>
+      <Route path='/admin' element={<AdminPage/>}/>
     </Routes>
     </BrowserRouter>
   )
