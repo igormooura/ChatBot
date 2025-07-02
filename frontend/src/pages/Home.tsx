e2f12a10c8e7be3e85f607e35f78119efb2e3830
igormooura <igor.oliveira.moura@gmail.com>
2025-07-02 16:08:37 -0300

    fix: o herosection tava muito grande

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,13 +4,13 @@ import HeroSection from "../components/HeroSection/HeroSection";
 
 const Home = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      
+      <main className="flex-grow">
         <HeroSection />
-      
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
