d0643d0c5ad6dff7b31df2a71b60df929a26256a
SouErick <ericksousasaraiva@gmail.com>
2025-07-11 21:13:53 -0300

    hotfix: relacionamento Patient-User

--- backend/app/models/Patient.py
+++ backend/app/models/Patient.py
@@ -8,7 +8,9 @@ class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     cpf = db.Column(db.String(14), unique=True, nullable=False)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, unique=True)
+    
+    # Isto define o outro lado da relação para agendamentos e logs de chat
     appointments = relationship('Appointment', back_populates='patient')
 
     def __repr__(self):
