69f090c536a5a741d42ea4ae12fe8f1578dfdd0e
Igor Moura <igor.oliveira.moura@gmail.com>
2025-07-15 21:51:26 -0300

    feat: coloquei um "simulador de bot" e adicionei as feats

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -2,38 +2,73 @@ import Background from "../components/Background/Background";
 import ChatMessage from "../components/Chat/ChatMessage";
 import Header from "../components/Header/Header";
 import ChatContainer from "../components/Chat/ChatContainer";
+import LoadingIndicator from "../components/Chat/LoadingIndicator";
+import ChatInputs from "../components/Chat/ChatInputs";
+import { useState } from "react";
 
-type Message = { 
+type Message = {
   id: number;
   sender: "user" | "bot";
   text: string;
   timestamp: string;
 };
 
-const sampleMessages: Message[] = [ //temporário
-  {
-    id: 1,
-    sender: "user",
-    text: "Oi, tudo bem?",
-    timestamp: "2025-07-15T19:05:00-03:00",
-  },
-  {
-    id: 2,
-    sender: "bot",
-    text: "Olá! Tudo ótimo e com você?",
-    timestamp: "2025-07-15T19:05:05-03:00",
-  },
-];;
-
 const ChatPage = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState("");
+
+  const handleSendMessage = () => {
+    if (!input.trim()) return;
+
+    const userMessage: Message = {
+      id: Date.now(),
+      sender: "user",
+      text: input,
+      timestamp: new Date().toISOString(),
+    };
+
+    setMessages((prev) => [...prev, userMessage]);
+    setInput("");
+    setIsLoading(true);
+
+    setTimeout(() => {
+      // pra simular o bot
+      const botMessage: Message = {
+        id: Date.now() + 1,
+        sender: "bot",
+        text: "Entendi! Em que mais posso ajudar?",
+        timestamp: new Date().toISOString(),
+      };
+      setMessages((prev) => [...prev, botMessage]);
+      setIsLoading(false);
+    }, 2000);
+  };
+
   return (
     <Background>
       <Header />
       <div className="flex justify-center">
         <ChatContainer>
-          {sampleMessages.map((msg) => (
-            <ChatMessage key={msg.id} messages={msg} />
-          ))}
+          {/* definitivamente nao sei se é uma boa pratica colocar esse 70vh */}
+          <div className="flex flex-col h-[70vh] max-w-5xl mx-auto"> 
+            <div className="flex-grow overflow-y-auto px-4 py-2">
+              {messages.map((msg) => (
+                <ChatMessage key={msg.id} messages={msg} />
+              ))}
+              {isLoading && (
+                <div className="flex mb-4 justify-start">
+                  <LoadingIndicator />
+                </div>
+              )}
+            </div>
+            <ChatInputs
+              input={input}
+              setInput={setInput}
+              loading={isLoading}
+              onSend={handleSendMessage}
+            />
+          </div>
         </ChatContainer>
       </div>
     </Background>
